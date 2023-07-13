4a23ed9358fe224bb7b395451baa11a93dcb667c
Terry <bengbengle@gmail.com>
2023-07-13 11:12:04 +0800

    add allowance check

--- test/multcoin.test.ts
+++ test/multcoin.test.ts
@@ -11,6 +11,7 @@ import { eth, Order, Side, setupTest } from '../exchange';
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { TransactionResponse } from "@ethersproject/providers";
 import { formatEther } from "ethers/lib/utils";
+import { MockERC20 } from "../typechain-types";
 
 
 const { ethers } = hre;
@@ -79,12 +80,24 @@ describe('MultiCoinTests', function () {
 
         // Verify 2. 如果没有授权，需要授权
         let _isApprovedForAll = await testNFT.connect(alice).isApprovedForAll(alice.address, executionDelegate.address);
-        console.log('_isApprovedForAll:', _isApprovedForAll);
         if (!_isApprovedForAll) {
             await testNFT.connect(alice).setApprovalForAll(executionDelegate.address, true);
         }
         assert(_isApprovedForAll, '_isApprovedForAll is false');
+
+
+        // Verify 3. check balance
+        let _balance = await (usdt as MockERC20).balanceOf(bob.address);
+        console.log('bob usdt _balance:', formatEther(_balance.toString()));
+
+         // Verify 4. check allowance
+        let _allowance = await (usdt as MockERC20).allowance(bob.address, executionDelegate.address);
+        if (!_allowance) {
+            await (usdt as MockERC20).connect(bob).approve(executionDelegate.address, eth('1000000'));
+        }
         
+        assert(_isApprovedForAll, '_allowance is false');
+
     });
 
     beforeEach(async () => {
@@ -98,7 +111,7 @@ describe('MultiCoinTests', function () {
         sell = generateOrder(alice, { side: Side.Sell, tokenId, paymentToken: usdt.address, price });
         buy = generateOrder(bob, { side: Side.Buy, tokenId, paymentToken: usdt.address, price });
 
-        console.log('tokenId:', tokenId);
+        assert(tokenId == 1, 'tokenId is false')
         sellInput = await sell.pack({ signer: alice });
         buyInput = await buy.pack();
 
@@ -110,17 +123,17 @@ describe('MultiCoinTests', function () {
         const pendingTx: TransactionResponse = await tx;
         const receipt = await pendingTx.wait();
 
-        console.log('receipt:', receipt.status);
-        assert(receipt.status, 'receipt.status is false');
+        // console.log('receipt:', receipt.status);
 
-        let alice_usdt =  await usdt.balanceOf(alice.address)
-        let bob_usdt =  await usdt.balanceOf(bob.address)
-        let admin_usdt =  await usdt.balanceOf(admin.address)
-        
-        console.log('alice usdt balance:', formatEther(alice_usdt.toString()));
-        console.log('bob usdt balance:',  formatEther(bob_usdt.toString()));
-        console.log('admin usdt balance:',  formatEther(admin_usdt.toString()));
-        
+        assert(receipt.status == 1, 'receipt.status is false');
+
+        let alice_usdt = await usdt.balanceOf(alice.address)
+        let bob_usdt = await usdt.balanceOf(bob.address)
+        let admin_usdt = await usdt.balanceOf(admin.address)
+
+        assert(formatEther(alice_usdt.toString()) == '1097.0', 'alice_usdt is false');
+        assert(formatEther(bob_usdt.toString()) == '900.0', 'bob_usdt is false');
+        assert(formatEther(admin_usdt.toString()) == '3.0', 'admin_usdt is false');
 
     });
 });
