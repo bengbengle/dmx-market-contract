48cc3511ad2d2da3c2a0760ee9245bedccba438b
Terry <bengbengle@gmail.com>
2023-07-13 10:55:43 +0800

    add solidity  Compiler settings

--- contracts/interfaces/IDMXExchange.sol
+++ contracts/interfaces/IDMXExchange.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 pragma abicoder v2;
 
 import {Input, Order} from "../lib/OrderStructs.sol";
--- contracts/interfaces/IExecutionDelegate.sol
+++ contracts/interfaces/IExecutionDelegate.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.13;
 
 interface IExecutionDelegate {
     function approveContract(address _contract) external;
